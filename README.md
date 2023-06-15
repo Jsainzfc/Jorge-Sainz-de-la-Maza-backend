879c20224169960281310875a7b332eb724ba875
Jorge Sainz <bodybluebloody@gmail.com>
2023-06-15 09:55:41 +0200

    Añadida validación de variables en la creación de un producto

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Alcance
 Se ha creado una clase Product con un constructor que inicializa todas las variables necesarias al valor introducido al crear una instancia.
 Se ha creado una clase ProductManager. Su constructor no recibe parámetros e inicializa un array de prodcutos vacío. Tiene varios métodos: 
-- addProduct: recibe todas las variables necesarias para la inicialización de un producto excepto el id. El id es una variable privada que crece secuencialmente. Valida además que el código del producto sea único a través de un método privado.
+- addProduct: recibe todas las variables necesarias para la inicialización de un producto excepto el id. El id es una variable privada que crece secuencialmente. Verifica que todos los campos están incluidos y además que el código del producto sea único a través de un método privado.
 - getProducts: devuelve un array de productos o un array vacío si el manager no tiene productos aún.
 - getProductById: devuelve el producto con un id determinado si lo encuentra o indica un error por consola.
 
--- index.js
+++ index.js
@@ -15,31 +15,38 @@ class ProductManager {
     this.products = []
   }
 
-  #lastId = 0
+  #lastId = 0 // Variable for storing the last available id (increments with every new product)
 
-  #codeExists (code) { //
+  #codeExists (code) { // True if there is already a product in the manager with the same code
     return this.products.findIndex(product => product.code === code) >= 0
   }
 
-  #getIndex (id) {
+  #getIndex (id) { // Returns the index in the products array of the product with that id or -1 if not found
     return this.products.findIndex (product => product.id === id)
   }
 
-  addProduct(code, title, description, price, stock, thumbnail) {
+  addProduct(code, title, description, price, stock, thumbnail) { // If correct creates a new Product and adds it to the array
+
+    if (!(code && title && description && price && stock && thumbnail)) {
+      console.error('Error! All of the fields must be included to create a new product.')
+      return
+    }
+
     if (this.#codeExists(code)) {
       console.error('Error! Product with the same code already exists, please add a unique code for the new product.')
       return
     }
+
     const product = new Product(this.#lastId, code, title, description, price, stock, thumbnail)
     this.products.push(product)
     this.#lastId++
   }
 
-  getProducts() {
+  getProducts() { // Returns the array of products
     return (this.products)
   }
   
-  getProductById(id) {
+  getProductById(id) { // Returns the product (if found) with that id
     const index = this.#getIndex(id)
     if (index >= 0) return this.products[index]
     console.error('Error! Product not found.') 
@@ -47,9 +54,10 @@ class ProductManager {
 }
 
 const manager = new ProductManager()
-console.log(manager.getProducts())
+console.log(manager.getProducts()) // Expect an empty array
 manager.addProduct ('abc123', 'producto prueba', 'Este es un product prueba', 200, 25, 'Sin imagen')
-console.log(manager.getProducts())
-manager.addProduct ('abc123', 'producto prueba', 'Este es un product prueba', 200, 25, 'Sin imagen')
-console.log(manager.getProductById(0))
-console.log(manager.getProductById(1))
+console.log(manager.getProducts()) // Expect an array with one item
+manager.addProduct ('abc123', 'producto prueba', 'Este es un product prueba', 200, 25, 'Sin imagen') // Expect a not unique error
+console.log(manager.getProductById(0)) // Expect product abc123
+console.log(manager.getProductById(1)) // Expect not found error
+manager.addProduct('abc123', 'producto prueba', 'Este es un product prueba', 200, 25) // Expect not all fields error
