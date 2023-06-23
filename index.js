e86b6d2fca39136555d3b664a70e7651106f2ed7
Jorge <bodybluebloody@gmail.com>
2023-06-23 12:24:46 +0200

    Entregable de la clase 4 terminado

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-class Product { // Objeto que guarda toda la información de los productos del carrito
+const fs = require('fs')
+// Module for managing files 
+
+class Product { // Class describing the product object which is stored in the ProductManager
   constructor (id, code, title, description, price, stock, thumbnail) {
     this.id = id
     this.code = code
@@ -11,53 +14,116 @@ class Product { // Objeto que guarda toda la información de los productos del c
 }
 
 class ProductManager {
-  constructor () { //Initializes an empty array of products
-    this.products = []
+  constructor (path) { // Initializes an empty array of products and the filesystem with the path given
+    this.path = path
+    this.#writeFile([])
+  }
+
+  #writeFile (products) {
+    fs.writeFileSync(this.path, JSON.stringify(products))
   }
 
   #lastId = 0 // Variable for storing the last available id (increments with every new product)
 
-  #codeExists (code) { // True if there is already a product in the manager with the same code
-    return this.products.findIndex(product => product.code === code) >= 0
+  #codeExists (products, code) { // True if there is already a product in the manager with the same code
+    return products.findIndex(product => product.code === code) >= 0
   }
 
-  #getIndex (id) { // Returns the index in the products array of the product with that id or -1 if not found
-    return this.products.findIndex (product => product.id === id)
+  #getIndex (products, id) { // Returns the index in the products array of the product with that id or -1 if not found
+    return products.findIndex (product => product.id === id)
   }
 
-  addProduct(code, title, description, price, stock, thumbnail) { // If correct creates a new Product and adds it to the array
+  async addProduct(code, title, description, price, stock, thumbnail) { // If correct creates a new Product and adds it to the array
 
     if (!(code && title && description && price && stock && thumbnail)) {
       console.error('Error! All of the fields must be included to create a new product.')
       return
     }
 
-    if (this.#codeExists(code)) {
+    const products = await this.getProducts()
+
+    if (this.#codeExists(products, code)) {
       console.error('Error! Product with the same code already exists, please add a unique code for the new product.')
       return
     }
 
     const product = new Product(this.#lastId, code, title, description, price, stock, thumbnail)
-    this.products.push(product)
+    products.push(product)
+    this.#writeFile(products)
     this.#lastId++
   }
 
-  getProducts() { // Returns the array of products
-    return (this.products)
+  async getProducts() { // Returns the array of products
+    const products = await fs.promises.readFile(this.path, 'utf-8')
+    return JSON.parse(products)
   }
   
-  getProductById(id) { // Returns the product (if found) with that id
-    const index = this.#getIndex(id)
-    if (index >= 0) return this.products[index]
+  async getProductById(id) { // Returns the product (if found) with that id
+    const products = await this.getProducts()
+    const index = this.#getIndex(products, id)
+    if (index >= 0) return products[index]
+    console.error('Error! Product not found.') 
+  }
+
+  async updateProduct(id, field, fieldValue) { // Updates one product of the products in the file
+    const products = await this.getProducts()
+    const index = this.#getIndex(products, id)
+    if (index >= 0) {
+      if (products[index].hasOwnProperty(field)) {
+        if (field === 'id') {
+          console.error('Id cannot be modified.')
+          return
+        }
+        products[index][field] = fieldValue
+        this.#writeFile(products)
+        return
+      }
+      console.error(`Field ${field} does not exist in product.`) 
+      return 
+    }
+    console.error('Error! Product not found.') 
+  }
+
+  async deleteProduct(id) {
+    const products = await this.getProducts()
+    const index = this.#getIndex(products, id)   
+    if (index >= 0) {
+      products.splice(index, 1)
+      this.#writeFile(products)
+      return
+    }
     console.error('Error! Product not found.') 
   }
 }
 
-const manager = new ProductManager()
-console.log(manager.getProducts()) // Expect an empty array
-manager.addProduct ('abc123', 'producto prueba', 'Este es un product prueba', 200, 25, 'Sin imagen')
-console.log(manager.getProducts()) // Expect an array with one item
-manager.addProduct ('abc123', 'producto prueba', 'Este es un product prueba', 200, 25, 'Sin imagen') // Expect a not unique error
-console.log(manager.getProductById(0)) // Expect product abc123
-console.log(manager.getProductById(1)) // Expect not found error
-manager.addProduct('abc123', 'producto prueba', 'Este es un product prueba', 200, 25) // Expect not all fields error
+testing = async () => {
+  const manager = new ProductManager('./productManager.txt')
+  let products = await manager.getProducts() 
+  console.log('Expect an empty array => ', products)
+  await manager.addProduct ('abc123', 'producto prueba', 'Este es un product prueba', 200, 25, 'Sin imagen')
+  products = await manager.getProducts()
+  console.log('Expect an array with only one item => ', products)
+  await manager.addProduct ('abc123', 'producto prueba', 'Este es un product prueba', 200, 25, 'Sin imagen') // Expect a not unique error
+  let product = await manager.getProductById(0)
+  console.log('Expect product abc123 => ', product)
+  product = await manager.getProductById(1) // Expect not found error
+  console.log('Expect undefined => ', product)
+  await manager.addProduct('abc123', 'producto prueba', 'Este es un product prueba', 200, 25) // Expect not all fields error
+  await manager.updateProduct(0, 'code', 'newCode')
+  product = await (manager.getProductById(0))
+  console.log(product)
+  await manager.updateProduct(1, 'code', 'newCode') // Expect not found error
+  await manager.updateProduct(0, 'codigo', 'newCode') // Expect field not existing error
+  await manager.updateProduct(0, 'id', '14') // Expect id cannot be modified error
+  await manager.addProduct ('code2', 'producto prueba', 'Este es un product prueba', 200, 25, 'Sin imagen')
+  products = await manager.getProducts()
+  console.log('Expect an array with 2 items => ', products)
+  await manager.deleteProduct(2) // Expect product not found error
+  products = await manager.getProducts()
+  console.log('Expect an array with 2 items => ', products)
+  await manager.deleteProduct(1)
+  products = await manager.getProducts()
+  console.log('Expect an array with only one item => ', products)
+}
+
+testing()
