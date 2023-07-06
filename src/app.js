bfa25dec72a0e3c16ec458670f7a3d2cd2c9f48b
Jsainzfc <120732505+Jsainzfc@users.noreply.github.com>
2023-07-06 13:21:18 +0000

    Cambios mínimos

--- src/app.js
+++ src/app.js
@@ -8,13 +8,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 // This line facilitates the server to read and manage long and complex urls.
 
-app.get('/', (req, res) => {
-  res.send(`Thanks for using this API. This endpoint is unused.
-  You may go to /products to retrieve all of the products present in the database.
-  You may use this endpoint also as /products?limit=x being x the maximum amount of products you want to retrieve.
-  You can also use /produtcs/:pid being pid the id of the products you want to retrieve.`)
-})
-
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
 
