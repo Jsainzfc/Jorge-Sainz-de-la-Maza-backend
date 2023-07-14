0a0cd83bd8631a6d145706eee1a6b5ab9902fed6
Jorge Sainz <jsainzfc@gmail.com>
2023-07-14 20:13:54 +0200

    Agregado uso de handlebars, refactorización del router y creada carpeta public para estilos

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 import express from 'express'
-import productsRouter from './routes/products.router.js'
-import cartsRouter from './routes/carts.router.js'
+import __dirname from './utils.js'
+import { engine } from 'express-handlebars'
+import {join} from 'path'
+import {api, home} from './routes/index.js'
 
 const app = express() // Initialize express app
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', join(__dirname, '/views'))
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 // This line facilitates the server to read and manage long and complex urls.
+app.use('/static', express.static(join(__dirname + '/public')))
 
-app.use('/api/products', productsRouter)
-app.use('/api/carts', cartsRouter)
+app.get('/', ((req, res) => {
+    res.render('index')
+}))
+
+app.use('/api', api)
+app.use('/', home)
 
 app.listen(8080, () => console.log(`Server up and listening in port 8080`))
 // Starting the server
